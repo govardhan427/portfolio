cdef83ba6f9f183314238e7d9d2a4d263fd03939
govardhan <ccgovardhan4@gmail.com>
2025-12-07 09:00:27 +0530

    backend

--- backend/build.sh
+++ backend/build.sh
@@ -10,5 +10,5 @@ python manage.py migrate
 
 echo "--- 3. Loading initial data fixture ---"
 python manage.py loaddata initial_data.json
-
+python manage.py shell -c "import os; from django.contrib.auth import get_user_model; User = get_user_model(); username=os.environ.get('DJANGO_SUPERUSER_USERNAME'); password=os.environ.get('DJANGO_SUPERUSER_PASSWORD'); email='ccgovardhan60@example.com'; User.objects.filter(username=username).exists() or User.objects.create_superuser(username, email, password)"
 echo "--- Build process complete ---"
