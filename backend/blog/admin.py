e43f26a4668993e3a5abc422337bfa071a4741e8
govardhan <ccgovardhan4@gmail.com>
2025-12-07 14:06:00 +0530

    backend

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import BlogPost
 
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
+    # --- Custom Method for Cloudinary Image Display (The Fix) ---
+    def image_tag(self, obj):
+        """Generates a working link to the Cloudinary image URL."""
+        if obj.cover_image:
+            # obj.cover_image.url now returns the correct absolute URL 
+            return mark_safe(f'<a href="{obj.cover_image.url}" target="_blank">View on Cloudinary</a>')
+        return "No Image"
+    image_tag.short_description = 'Image Link'
+
     # What columns to show in the list
-    list_display = ('title', 'is_published', 'created_at', 'read_time')
+    list_display = ('title', 'is_published', 'created_at', 'read_time', 'image_tag')
     
     # Sidebar filters
     list_filter = ('is_published', 'created_at')
@@ -18,7 +28,8 @@ class BlogPostAdmin(admin.ModelAdmin):
     # Layout adjustment for better writing experience
     fieldsets = (
         (None, {
-            'fields': ('title', 'slug', 'cover_image')
+            # Inject image_tag here for direct display next to the file field
+            'fields': ('title', 'slug', 'cover_image', 'image_tag') 
         }),
         ('Content', {
             'fields': ('content', 'tags')
@@ -27,3 +38,6 @@ class BlogPostAdmin(admin.ModelAdmin):
             'fields': ('is_published', 'read_time')
         }),
     )
+    
+    # CRITICAL: Mark image_tag as read-only
+    readonly_fields = ('image_tag',)
